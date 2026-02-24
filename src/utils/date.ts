31dbf7d58c55af8b13698b28fc9363225306b338
Martin Morgan <mtmorgan.xyz@gmail.com>
2026-02-24 16:10:39 -0500

    extract YYYY-MM-DD from Date pedantically

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,8 @@
-export const formatDate = (timestamp: number): string =>
-  new Date(timestamp).toISOString().split("T")[0];
+export const formatDate = (timestamp: number): string => {
+  const date = new Date(timestamp);
+  const offset = date.getTimezoneOffset() * 60000; // offset in milliseconds
+  return new Date(date.getTime() - offset).toISOString().split("T")[0];
+};
 
 export const formatHour = (timestamp: number): string =>
   new Date(timestamp).getHours().toString();
